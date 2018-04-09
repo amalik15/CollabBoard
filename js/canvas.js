1d39acf4e2ce93bd3086640c2f582bcd81368d03
mynameisabdul <amalik11@uic.edu>
2018-04-08 21:39:12 -0500

    drawable canvas with basic functionality

---
+++ js/canvas.js
@@ -0,0 +1,86 @@
+//set up the canvas
+var canvas = document.querySelector('#drawingCanvas');
+var ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#000000';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+//set pen length
+var lineWidth = 1;
+ctx.lineWidth = lineWidth;
+
+//temp varables
+var isDrawing = false;
+var lastX = 0;
+var lastY = 0;
+
+//function to draw on canvas
+function draw(e){
+    
+    //returns if user is not clicking
+    if(!isDrawing){
+        return;
+    }
+
+    //draw on canvas
+    ctx.beginPath();
+    ctx.moveTo(lastX,lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+}
+
+//mouse events
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
+
+//get refrance to textfild
+var lineTextFild = document.getElementById("penSizeTextFild");
+lineTextFild.value = lineWidth;
+
+//increase the width of the pen
+function plus(){
+    lineWidth++;
+    lineTextFild.value = lineWidth;
+    console.log(lineWidth);
+    ctx.lineWidth = lineWidth;
+}
+
+//decrease the width of the pen
+function minus(){
+    if(lineWidth >= 2 ){
+        lineWidth--;
+        lineTextFild.value = lineWidth;
+        console.log(lineWidth);
+        ctx.lineWidth = lineWidth;
+    }   
+}
+
+//called when text fild is updated. if the number inputed is valed it will update the pen lingth
+function lineTextFildEditor(){
+    let num = Number(lineTextFild.value);
+    if(isNaN(num) || num < 1 ){
+        lineTextFild.value = lineWidth;
+        return;
+    }
+    lineWidth = num;
+    ctx.lineWidth = lineWidth;
+}
+
+//clear canvas
+function clearCanvas(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+//change painting color
+function colorChanger(color){
+    ctx.strokeStyle = color;
+}
+
